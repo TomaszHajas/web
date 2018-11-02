f282c7d04b151f5a919216004def734a873810c7
TomaszHajas <203393@student.pwr.edu.pl>
2018-11-02 21:05:48 +0100

    Update addTemplate.php

--- randomizer/addTemplate.php
+++ randomizer/addTemplate.php
@@ -2,7 +2,7 @@
   $template = $_POST['template'];
   $file = 'templates.txt';
   $currentContent = file_get_contents($file);
-  $currentContent .= $template;
+  $currentContent .= $template+"\n";
   file_put_contents($file, $currentContent);
   header("Location: {$_SERVER['HTTP_REFERER']}");
   exit;
