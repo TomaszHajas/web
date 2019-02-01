38fd4725f04627f51271957ba73983eb86810f1c
TomaszHajas <tomaszhajas@gmail.com>
2019-02-01 16:00:27 +0100

    Update widget.php

--- wordpress/wp-content/plugins/date-and-time-widget/views/widget.php
+++ wordpress/wp-content/plugins/date-and-time-widget/views/widget.php
@@ -3,6 +3,7 @@
   background-color: <?php echo $background_color ?>;
   font-family: <?php echo $font_family ?>;
   font-size: <?php echo $font_size ?>;">
+  <h4 class="widget-title">GMT+1 Time</div>
   <div class="date"></div>
   <div class="time"></div>
 </div>
