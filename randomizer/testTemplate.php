627263726337d85f34d936d7da6a20b54d34955c
TomaszHajas <tomaszhajas@gmail.com>
2019-03-24 10:09:35 +0100

    Update testTemplate.php

--- randomizer/testTemplate.php
+++ randomizer/testTemplate.php
@@ -1,11 +1,5 @@
 <?php
   $template = $_POST['template'];
-  if (empty($template)) {
-      echo 'Please correct the template field';
-      return false;
-  }else {
-      header("Location: http://tomaszhajas.azurewebsites.net/randomizer/index.html"."?template=".urlencode($template));
-
-  }
+  header("Location: http://tomaszhajas.azurewebsites.net/randomizer/index.html"."?template=".urlencode($template));
   exit;
 ?>
