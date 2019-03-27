08fbe6a3e3926547306393b4c215ed263dbee7b8
TomaszHajas <tomaszhajas@gmail.com>
2019-03-27 19:39:43 +0100

    Update testTemplate.php

--- randomizer/testTemplate.php
+++ randomizer/testTemplate.php
@@ -1,4 +1,5 @@
 <?php
+
   $template = $_POST['template'];
   $seed = mt_rand(1000000, 9999999);
   header("Location: http://tomaszhajas.azurewebsites.net/randomizer/index.html"."?template=".rawurlencode($template)."&seed=".$seed);
