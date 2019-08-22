7b345549d7250850822ae0249856fe029b4474df
TomaszHajas <tomaszhajas@gmail.com>
2019-08-22 23:00:55 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -15,15 +15,9 @@ module.exports = function (app) {
         res.render('about', { title: 'About Me.  ' })
     });
     
-    // randomizer page
-    app.get('/randomizer*', function(req, res){
-        console.log(res.query);
-        console.log(req.query.seed);
-        res.render({seed: req.query.seed}, res);
-    });
     
     // wordpress page
     app.get('/wordpress', function(req, res){
-        res.render(http://tomaszhajas.azurewebsites.net/wordpress);
+        res.render('wordpress', { title: 'Wordpress  ' })
     });
 }
