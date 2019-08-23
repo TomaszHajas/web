bad018e31763132384ab32b796cd9305d6c7ccc1
TomaszHajas <tomaszhajas@gmail.com>
2019-08-23 10:50:38 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -18,7 +18,7 @@ module.exports = function (app) {
     
     // randomizer page
     app.get('/randomizer*', function(req, res){
-        res.end(path.resolve(__dirname+'/randomizer/index.html'));
+        res.end("test")
         //res.sendfile(path.resolve(__dirname+'/randomizer/index.html'))
     });
 }
