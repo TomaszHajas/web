6529dee7aa65a3205d7beca1d93262f9a0716831
TomaszHajas <tomaszhajas@gmail.com>
2019-08-23 11:09:55 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
-    app.use('/randomizer', express.static(__dirname));
+    path = require('path')
     // home page
     app.get('/', function (req, res) {
         res.render('index', { title: 'Home Page.  ' })
@@ -17,7 +17,7 @@ module.exports = function (app) {
     
     // randomizer page
     app.get('/randomizer*', function(req, res){
-        res.send(__dirname)
+        res.send(path.join(__dirname, '../'))
         //res.sendfile(path.resolve(__dirname+'/randomizer/index.html'))
     });
 }
