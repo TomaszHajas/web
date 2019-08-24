4755df4e821a82ef331374797598ffb02dfbd542
TomaszHajas <tomaszhajas@gmail.com>
2019-08-24 21:22:06 +0200

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -20,34 +20,4 @@ module.exports = function (app) {
     app.get('/randomizer', function(req, res){
         res.sendFile('/randomizer/index.html')
     });
-    
-    // wordpress
-    app.get('/wordpress/post', function(req, res){
-        var headers, options;
-
-        // Set the headers
-        headers = {
-            'Content-Type':'application/x-www-form-urlencoded'
-        }
-
-        // Configure the request
-        options = {
-         url: 'http://tomaszhajas.azurewebsites.net/wordpress/wp-json/wp/v2/posts/1',
-            method: 'GET',
-            headers: headers
-        }
-
-        // Start the request
-        request(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                res.send({
-                 success: true,
-                message: "Successfully fetched a list of post", 
-                posts: JSON.parse(body)
-             });
-            } else {
-                 console.log(error);
-            }
-        });
-    }
 }
